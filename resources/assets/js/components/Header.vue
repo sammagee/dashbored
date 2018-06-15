f48c6e48aa0ebb67f412f8a300edaca2aa3d5f73
Sam Magee <s@sammagee.me>
2018-06-14 22:40:50 -0500

    IMPROVEMENT: Add mobile settings menu

--- resources/assets/js/components/Header.vue
+++ resources/assets/js/components/Header.vue
@@ -1,23 +1,31 @@
 <template>
-  <header class="bg-white flex h-16 items-center mb-5 px-5 pr-16 md:pr-5 shadow">
-    <div class="flex flex-1 items-center mr-auto">
-      <router-link
-        :to="{ name: 'home' }"
-        class="leading-0">
-        <logo class="w-8" />
-      </router-link>
+  <header class="bg-white mb-5 shadow">
+    <div class="flex h-16 items-center px-5 pr-16">
+      <div class="flex flex-1 items-center mr-auto">
+        <router-link
+          :to="{ name: 'home' }"
+          class="leading-0">
+          <logo class="w-8" />
+        </router-link>
 
-      <h1 class="flex items-center leading-none text-black text-xl">
-        <span class="mx-2 text-xs text-grey">/</span>
-        <slot name="title"></slot>
-      </h1>
+        <h1 class="flex items-center leading-none text-black text-xl">
+          <span class="mx-2 text-xs text-grey">/</span>
+          <slot name="title"></slot>
+        </h1>
+      </div>
+
+      <nav class="hidden md:flex flex-1 h-16 md:justify-center mx-auto -mb-px">
+        <slot name="nav"></slot>
+      </nav>
+
+      <div class="flex-1 ml-auto"></div>
     </div>
 
-    <nav class="flex flex-1 h-16 md:justify-center mx-auto -mb-px">
-      <slot name="nav"></slot>
-    </nav>
-
-    <div class="flex-1 ml-auto"></div>
+    <div class="flex md:hidden h-16 items-center px-5 overflow-x-scroll">
+      <nav class="flex flex-1 h-16 md:justify-center mx-auto -mb-px">
+        <slot name="nav-mobile"></slot>
+      </nav>
+    </div>
   </header>
 </template>
 
